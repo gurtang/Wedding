99c556e7d6856d5b64b18bb5cdb3389108f3dd46
Slobodan Milosevic <miosevicslobodanpk@gmail.com>
2026-05-06 15:51:31 +0200

    Robots izmenjen za OG tagove za metu

---
+++ app/robots.ts
@@ -0,0 +1,25 @@
+import type { MetadataRoute } from "next";
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: [
+      {
+        userAgent: "*",
+        allow: "/",
+      },
+      {
+        userAgent: "facebookexternalhit",
+        allow: "/",
+      },
+      {
+        userAgent: "Facebot",
+        allow: "/",
+      },
+      {
+        userAgent: "meta-externalagent",
+        allow: "/",
+      },
+    ],
+    sitemap: "/sitemap.xml",
+  };
+}
